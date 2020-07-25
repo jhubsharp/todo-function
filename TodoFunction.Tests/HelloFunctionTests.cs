d3602ec119e115edf17e81bef565ef77e549da71
Shawn Hubbard <shawn.hubbard@hubsharp.com>
2020-07-25 11:25:51 -0500

    Adding tests

---
+++ TodoFunction.Tests/HelloFunctionTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Primitives;
+using Shouldly;
+using System.Collections.Generic;
+using Xunit;
+
+namespace TodoFunction.Tests
+{
+    public class HelloFunctionTests
+    {
+        private readonly ILogger logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+        
+        [Fact]
+        public async void HttpTriggerWithoutParamsReturnsGenericMessage()
+        {
+            var query = new Dictionary<string, StringValues>();
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection(query)
+            };
+
+            var response = (OkObjectResult)await HelloFunction.Run(request, logger);
+
+            response.Value.ShouldBe("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.");
+        }
+
+        [Fact]
+        public async void HttpTriggerWithNameParamShouldReturnNameInResponse()
+        {
+            var query = new Dictionary<string, StringValues>();
+            query.Add("name", "Robert");
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection(query)
+            };
+
+            var response = (OkObjectResult)await HelloFunction.Run(request, logger);
+
+            response.Value.ShouldBe("Hello, Robert. This HTTP triggered function executed successfully.");
+        }
+    }
+}
--- TodoFunction/HelloFunction.cs
+++ TodoFunction/HelloFunction.cs
@@ -10,9 +10,9 @@ using Newtonsoft.Json;
 
 namespace TodoFunction
 {
-    public static class Function1
+    public static class HelloFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("HelloFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
